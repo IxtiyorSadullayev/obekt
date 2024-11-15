f198d326946fa7a0f1fa2bd77085f3f0aad23c2c
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2024-11-15 15:14:06 +0500

    Sinf va Obekt yaratish

---
+++ sinf.py
@@ -0,0 +1,33 @@
+class Oquvchi:
+    name: str
+    fam: str
+    age: int
+    manzil: str
+
+class Car:
+    color: str
+    model: str
+    number: str
+
+class Hona:
+    name: str 
+    hona_namber: int
+
+class Komputer:
+    brend: str
+    ram: str 
+    rom: str
+    protsessor: str
+    video_card: str
+
+class Telefon: 
+    brend:str
+    ram:str
+    rom:str
+    # ..
+
+class Birinchi_sinf:
+    name: str 
+    count_pupil: int
+    pupils: list
+    # 
---
+++ sinf_obekt.py
@@ -0,0 +1,28 @@
+class Oquvchi:
+    name: str
+    fam: str
+    age: int
+    manzil: str
+
+    def __init__(self, name:str, fam:str, age:int, manzil:str):
+        self.name=name
+        self.fam=fam
+        self.age=age
+        self.manzil=manzil
+
+rasul=Oquvchi("Rasul", "Baxtiyorov", 16, "O'zbekiston")
+# rasul.name="Rasul"
+# rasul.fam="Baxtiyorov"
+# rasul.age=16
+# rasul.manzil="O'zbekiston OFY"
+
+bexruz=Oquvchi("Bexruz", "Erimbetov", 16, "MFY and OFY")
+# bexruz.name="Bexruz"
+# bexruz.fam="Erimbetov"
+# bexruz.age=16
+# bexruz.manzil="MFY and OFY"
+
+print(rasul.name,bexruz.name)
+yunus = Oquvchi("Yunus", "Jumanazarov", 17, "Istiqlol")
+mohinur = Oquvchi("Mohinur", "Aitboyeva", 15, "Mustaqillik MFY")
+print(yunus.fam, mohinur.fam)
